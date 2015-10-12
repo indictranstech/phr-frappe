921a5ea0bcda390aad70b719c023bd9f2b8b2700
pawaranand <pawaranand00@gmail.com>
2015-10-12 17:31:58 +0530

    [Fix]Social Login

--- frappe/templates/pages/login.py
+++ frappe/templates/pages/login.py
@@ -167,7 +167,7 @@ def login_oauth_user(data, provider=None):
 		user = data["email"]
 		user_type = frappe.db.get_value("User",{"email":user},"access_type")
 		if user_type == 'Provider':
-			return frappe.respond_as_web_page("Not Allowed", """<p style="margin-top:10%;text-align: center;"">Sorry. Provider Cant Use this Functionality.</p><p style="text-align: center;">""",
+			return frappe.respond_as_web_page("Not Allowed", """<p style="margin-top:10%;text-align: center;"">Sorry. Provider Cant Use this Functionality.</p><p style="text-align: center;"><a href='index'>Back to Home</a></p>""",
 				success=False, http_status_code=403)
 
 		try:
